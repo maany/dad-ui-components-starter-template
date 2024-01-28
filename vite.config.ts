cc48222fb3f1ba292bf2afcdd3690b41e76466a3
maany <imptodefeat@gmail.com>
2024-01-29 00:19:09 +0100

    lint and format

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import dts from "vite-plugin-dts";
+import path from "path";
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig, UserConfig } from "vite";
 
-// https://vitejs.dev/config/
+const MODULE_NAME = "dad-ui-component-library";
+
 export default defineConfig({
-  plugins: [react()],
-})
+  base: "./",
+  plugins: [dts({ rollupTypes: true }), react()],
+  build: {
+    sourcemap: true,
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      name: MODULE_NAME,
+      formats: ["es", "cjs", "umd", "iife"],
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+} satisfies UserConfig);

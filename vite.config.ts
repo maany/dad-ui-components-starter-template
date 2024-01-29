d6f14bcb75e3a9ad4ff9b9e86e5c97499e675c49
maany <imptodefeat@gmail.com>
2024-01-29 01:56:55 +0100

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,24 +3,43 @@ import { libInjectCss } from "vite-plugin-lib-inject-css";
 import path from "path";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig, UserConfig } from "vite";
+import { glob } from 'glob';
+import { extname, relative, resolve } from 'path'
+import { fileURLToPath } from 'node:url'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
-  plugins: [dts({ rollupTypes: true }), react(), libInjectCss()],
+  plugins: [dts({ include: ['lib'] }), react(), libInjectCss()],
   build: {
     copyPublicDir: false,
     sourcemap: true,
     lib: {
-      entry: path.resolve(__dirname, "src/index.ts"),
+      entry: path.resolve(__dirname, "lib/main.ts"),
       formats: ["es"],
     },
     rollupOptions: {
-      external: ["react", "react-dom"],
+      external: ["react", "'react/jsx-runtime'"],
+      input: Object.fromEntries(
+        // https://rollupjs.org/configuration-options/#input
+        glob.sync('lib/**/*.{ts,tsx}').map(file => [
+          // 1. The name of the entry point
+          // lib/nested/foo.js becomes nested/foo
+          relative(
+            'lib',
+            file.slice(0, file.length - extname(file).length)
+          ),
+          // 2. The absolute path to the entry file
+          // lib/nested/foo.ts becomes /project/lib/nested/foo.ts
+          fileURLToPath(new URL(file, import.meta.url))
+        ])
+      ),
       output: {
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
-        },
+        // globals: {
+        //   react: "React",
+        //   "react-dom": "ReactDOM",
+        // },
+        assetFileNames: 'assets/[name][extname]',
+        entryFileNames: '[name].js',
       },
     },
   },

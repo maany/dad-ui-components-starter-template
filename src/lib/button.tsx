827b5c4b07f8f5e133b704a59976dc7f0261d6c9
maany <imptodefeat@gmail.com>
2024-01-29 01:31:12 +0100

    add a lib folder

---
+++ src/lib/button.tsx
@@ -0,0 +1,5 @@
+export function Button(props: React.ButtonHTMLAttributes<HTMLButtonElement>) {
+  return <button {...props} />;
+}
+
+export default Button;
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,18 @@
 import dts from "vite-plugin-dts";
+import { libInjectCss } from "vite-plugin-lib-inject-css";
 import path from "path";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig, UserConfig } from "vite";
 
 export default defineConfig({
   base: "./",
-  plugins: [dts({ rollupTypes: true }), react()],
+  plugins: [dts({ rollupTypes: true }), react(), libInjectCss()],
   build: {
+    copyPublicDir: false,
     sourcemap: true,
     lib: {
       entry: path.resolve(__dirname, "src/index.ts"),
       formats: ["es"],
-      fileName: (format) => `index.${format}.js`,
     },
     rollupOptions: {
       external: ["react", "react-dom"],

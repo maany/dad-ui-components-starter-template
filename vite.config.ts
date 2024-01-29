845abf994755b8802a086a6ac8dad0818b317928
maany <imptodefeat@gmail.com>
2024-01-29 02:15:05 +0100

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,15 @@ import { libInjectCss } from "vite-plugin-lib-inject-css";
 import path from "path";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig, UserConfig } from "vite";
-import { glob } from 'glob';
-import { extname, relative, resolve } from 'path'
-import { fileURLToPath } from 'node:url'
+import { glob } from "glob";
+import { extname, relative } from "path";
+import { fileURLToPath } from "node:url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [dts({ include: ['lib'] }), react(), libInjectCss()],
+  plugins: [dts({ include: ["lib"] }), react(), libInjectCss()],
   build: {
     copyPublicDir: false,
-    sourcemap: true,
     lib: {
       entry: path.resolve(__dirname, "lib/main.ts"),
       formats: ["es"],
@@ -21,25 +20,18 @@ export default defineConfig({
       external: ["react", "'react/jsx-runtime'"],
       input: Object.fromEntries(
         // https://rollupjs.org/configuration-options/#input
-        glob.sync('lib/**/*.{ts,tsx}').map(file => [
+        glob.sync("lib/**/*.{ts,tsx}").map((file) => [
           // 1. The name of the entry point
           // lib/nested/foo.js becomes nested/foo
-          relative(
-            'lib',
-            file.slice(0, file.length - extname(file).length)
-          ),
+          relative("lib", file.slice(0, file.length - extname(file).length)),
           // 2. The absolute path to the entry file
           // lib/nested/foo.ts becomes /project/lib/nested/foo.ts
-          fileURLToPath(new URL(file, import.meta.url))
-        ])
+          fileURLToPath(new URL(file, import.meta.url)),
+        ]),
       ),
       output: {
-        // globals: {
-        //   react: "React",
-        //   "react-dom": "ReactDOM",
-        // },
-        assetFileNames: 'assets/[name][extname]',
-        entryFileNames: '[name].js',
+        assetFileNames: "assets/[name][extname]",
+        entryFileNames: "[name].js",
       },
     },
   },
